e66d58c29cd666ada311809d88d8344e63f03ea2
EricHeredia <EricDavidHeredia@gmail.com>
2019-05-07 01:39:39 -0500

    Updates

---
+++ precache-manifest.d68ccf5ff247954b14c7dd76028683c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7decd225a2de6cbe7de749f04b22e3ea",
+    "url": "/local-eats/index.html"
+  },
+  {
+    "revision": "ffac92938fe6d60dfd99",
+    "url": "/local-eats/static/css/main.c5276d69.chunk.css"
+  },
+  {
+    "revision": "18f014136ed9036c82c7",
+    "url": "/local-eats/static/js/2.f0275988.chunk.js"
+  },
+  {
+    "revision": "ffac92938fe6d60dfd99",
+    "url": "/local-eats/static/js/main.073ee130.chunk.js"
+  },
+  {
+    "revision": "6c2c2f1dd400bd50a97d",
+    "url": "/local-eats/static/js/runtime~main.b331fef2.js"
+  }
+]);
